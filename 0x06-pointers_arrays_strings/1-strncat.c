fce799f3bb1ddd1666071f221a3491129fcccae7
troyCodez <woodtroy32@yahoo.com>
2023-03-31 10:25:13 -0700

    hot fix

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,5 @@
-include "main.h"
+#include "main.h"
+#include <string.h>
 /**
  * _strncat - concatenates two strings
  * @dest: string input
@@ -9,25 +10,7 @@ include "main.h"
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int i;
-	int j;
-
-	i = 0;
-
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
-	j = 0;
-
-	while (j < n && src[j] != '\0')
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
-	dest[i] = '\0';
+	strncat (dest, src, n);
 	return (dest);
-
 
 }
--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,4 +1,4 @@
-include "main.h"
+#include "main.h"
 /**
  * _strncpy - function copies a string
  * @dest: input value
