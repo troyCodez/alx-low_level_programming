e402bd9197474f3b7676f7afd45d0285070b3350
troyCodez <woodtroy32@yahoo.com>
2023-04-03 17:04:34 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _memset - function fills memory with n byte
+ * @n:input byte
+ * @s:strating adrress
+ * @b:output results
+ * Return: array with new values
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+	for (i = 0;n > 0;i++)
+	{
+		s[i] = b;
+		
+		n--;
+	}
+
+	return (s);
+
+}
